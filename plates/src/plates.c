d52eecadff98b79599377fd572f845e51bacfc9a
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-05-11 17:18:43 +0200

    c

--- plates/src/plates.c
+++ plates/src/plates.c
@@ -41,34 +41,6 @@ static bool is_old_plate(char* plate){
 	return strlen(plate)==OLD_PLATE_LENGTH&&is_valid_old_plate_chars(plate);
 }
 
-/**
- * Restituisce il tipo di targa.
- * La funzione riconosce due soli tipi di targa: vecchio tipo o nuovo tipo.
- * Per targhe di vecchio tipo, il metodo restituisce OLD_PLATE; per targhe di nuovo tipo
- * il metodo restituisce NEW_PLATE.
- * Se la funzione non riconosce il tipo di targa, restituisce
- * UNRECOGNIZED.
- * <p>
- * TARGHE DI VECCHIO TIPO <br>
- * Una targa di vecchio tipo e' costituita da 8 caratteri. I primi due caratteri sono
- * lettere, gli ultimi
- * cinque caratteri sono cifre. Il terzo carattere puo' essere lettera o cifra.
- * <p>
- * TARGHE DI NUOVO TIPO <br>
- * Una targa di nuovo tipo e' costituita da 7 caratteri.
- * I primi due caratteri e gli ultimi due caratteri
- * sono lettere AMMESSE. I tre caratteri centrali sono cifre.
- * Le lettere ammesse sono tutte le lettere
- * dell'alfabeto TRANNE: GIOQU
- * <p>
- * @pre plate non puo' essere null e deve essere costituita esclusivamente da caratteri
- * alfanumerici.
- * @post il valore restituito puo' essere solo OLD_PLATE, NEW_PLATE oppure
- * UNRECOGNIZED.
- * <p>
- * @param[in] plate la targa automobilistica
- * @return il tipo di targa
- */
 plate_t check_plate(char* plate){
 	assert(plate!=NULL);
 	plate_t result=is_new_plate(plate)?NEW_PLATE:is_old_plate(plate)?OLD_PLATE:UNRECOGNIZED;

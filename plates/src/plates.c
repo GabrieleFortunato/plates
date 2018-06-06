06059db7444c615fbe344154af8e3f6f13caafa3
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-06-06 15:00:40 +0200

    Commit

--- plates/src/plates.c
+++ plates/src/plates.c
@@ -14,7 +14,7 @@ static bool is_valid_plate(char* plate){
 	return flag;
 }
 
-static bool are_valid_char_old_plate(char* plate){
+static bool are_valid_chars_old_plate(char* plate){
 	bool flag = true;
 	for (int i=ZERO;i<strlen(plate)&&flag;i++)
 		flag = (i==ZERO||i==ONE)?isalpha(plate[i]):(i==TWO)?isalnum(plate[i]):isdigit(plate[i]);
@@ -22,28 +22,28 @@ static bool are_valid_char_old_plate(char* plate){
 }
 
 static bool is_old_plate(char* plate){
-	return strlen(plate)==OLD_PLATE_LENGTH&&are_valid_char_old_plate(plate);
+	return strlen(plate)==OLD_LENGTH&&are_valid_chars_old_plate(plate);
 }
 
-static bool is_ok_alpha(char a){
+static bool is_valid_alpha(char a){
 	return isalpha(a)&&a!=G&&a!=I&&a!=O&&a!=Q&&a!=U;
 }
 
-static bool are_valid_char_new_plate(char* plate){
+static bool are_valid_chars_new_plate(char* plate){
 	bool flag = true;
 	for (int i=ZERO;i<strlen(plate)&&flag;i++)
-		flag = (i==ZERO||i==ONE||i==FIVE||i==SIX)?is_ok_alpha(plate[i]):isdigit(plate[i]);
+		flag = (i==ZERO||i==ONE||i==FIVE||i==SIX)?is_valid_alpha(plate[i]):isdigit(plate[i]);
 	return flag;
 }
 
-static bool is_new_plate(char* plate){
-	return strlen(plate)==NEW_PLATE_LENGTH&&are_valid_char_new_plate(plate);
+bool is_new_plate(char* plate){
+	return strlen(plate)==NEW_LENGTH&&are_valid_chars_new_plate(plate);
 }
 
 plate_t check_plate(char* plate){
 	assert(plate!=NULL);
 	assert(is_valid_plate(plate));
-	plate_t result = is_old_plate(plate)?OLD_PLATE:is_new_plate(plate)?NEW_PLATE:UNRECOGNIZED;
+	plate_t result = (is_old_plate(plate))?OLD_PLATE:(is_new_plate(plate))?NEW_PLATE:UNRECOGNIZED;
 	assert(result==OLD_PLATE||result==NEW_PLATE||result==UNRECOGNIZED);
 	return result;
 }
